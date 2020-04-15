1bb10e6cd9a9bff2ccb241ee6f1253db3134b1ba
Mike Burnside <mburnside@mburnside-ltm2.internal.salesforce.com>
2020-04-15 11:02:51 +1000

    test

--- server.js
+++ server.js
@@ -64,10 +64,7 @@ var consumer = subscriptionClient.subscribe(eventData => {
   // Do something on receipt of the event
   console.log("Received event: ");
 
-var aestTime = new Date().toLocaleString('en-AU', {timeZone: 'Australia/Melbourne'});
-console.log(aestTime);
-
-  var rightnow= new Date().toLocaleString('en-AU',{timeZone: 'Australia/Melbourne'});
+  var rightnow= new Date().toLocaleTimeString('en-AU',{timeZone: 'Australia/Melbourne'});
   this.myevent={"timestamp":rightnow,"eventData":eventData};
   console.log(JSON.stringify(eventData, null, 2));
 }, (err) => {
